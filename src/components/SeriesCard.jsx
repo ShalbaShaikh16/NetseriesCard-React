2d3830a682adc17c02e98c45d740edd6002ee67a
Shalba Shaikh <159912649+ShalbaShaikh16@users.noreply.github.com>
2025-12-29 23:16:33 +0530

    Update SeriesCard.jsx

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -2,37 +2,48 @@ import React from 'react'
 import styles from "./Netflix.module.css";
 
 const SeriesCard = ({ data }) => {
-  //console.log(props);
-  //destructuring
   const { img_url, name, rating, description, cast, genre, watch_url } = data;
-  const Rating = rating >= 8.5 ? styles.super: styles.less;
-  const btn_styl = {                      //Inline Css
+
+  const Rating = rating >= 8.5 ? styles.super : styles.less;
+
+  const btn_styl = {
     padding: "1.2rem 2.4rem",
     border: "none",
     fontSize: "1.6rem",
-    backgroundColor: `${rating >= 8.5 ? "green" : "red"}`,
+    backgroundColor: rating >= 8.5 ? "green" : "red",
     color: "#000",
     fontWeight: "bold",
     cursor: "pointer",
     margin: "3.2rem",
-  }
+  };
+
   return (
-    <li className={styles.card} >
+    <li className={styles.card}>
       <div>
-        <img src={img_url} alt="rings.jpg" width={"40%"} height={"50%"} />
+       
+        <img src={img_url} alt={name} width="40%" height="50%" />
       </div>
+
       <div className={styles["card-content"]}>
-        <h2>Name:{name}</h2>
-        <h3>Rating <span className={`${styles.rating} ${Rating}`}>{rating}</span></h3>
-        <p>Summary:{description}</p>
+        <h2>Name: {name}</h2>
+
+        <h3>
+          Rating{" "}
+          <span className={`${styles.rating} ${Rating}`}>
+            {rating}
+          </span>
+        </h3>
+
+        <p>Summary: {description}</p>
         <p>Genre: {genre.join(", ")}</p>
         <p>Cast: {cast.join(", ")}</p>
-        <a href={watch_url} target="_blank">
-          <button style={btn_styl}>Watch Now</button> </a>
+
+        <a href={watch_url} target="_blank" rel="noreferrer">
+          <button style={btn_styl}>Watch Now</button>
+        </a>
       </div>
     </li>
   );
-}
-//ctrl+shift+L
+};
 
-export default SeriesCard
+export default SeriesCard;
